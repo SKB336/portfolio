9ff9f3e112b9c1fe19b6884ffd70cebdf018241a
ShakeebIVIS <shakeeb@intelliversal.com>
2025-07-11 17:15:50 +0500

    [WIP] Clients Section

---
+++ components/Clients.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Clients = () => {
+  return (
+    <div className='py-20' id="projects">
+      <h1 className='heading'>
+        Kind words from {` `}
+        <span className='text-purple'>
+            satisfied clients
+        </span>
+      </h1>
+      <div className='flex flex-col items-center justify-center p-4 gap-x-24 gap-y-8 mt-10 '>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Clients
