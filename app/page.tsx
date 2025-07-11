e4993571cbbe2f8bda7dad3a7fa2053872dc07be
ShakeebIVIS <shakeeb@intelliversal.com>
2025-07-11 15:52:12 +0500

    [ADD] Projects Section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
@@ -10,11 +12,10 @@ export default function Home() {
       overflow-hidden mx-auto sm:px-10 px-5"
     >
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome/>}
-        ]}/>
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4ce35f91f468896c716117e36c19ec6d@o4509649324277760.ingest.de.sentry.io/4509649326374992",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
