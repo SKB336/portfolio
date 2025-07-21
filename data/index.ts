d216f922e9f22742ec1359a812f153264dba0cb1
ShakeebIVIS <shakeeb@intelliversal.com>
2025-07-21 16:02:46 +0500

    [ADD] Information 1

--- data/index.ts
+++ data/index.ts
@@ -75,7 +75,7 @@ export const projects = [
       des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
       img: "/p1.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      link: "https://github.com/SKB336/react-native/tree/main/cv-maker-v4",
     },
     {
       id: 2,
@@ -83,7 +83,7 @@ export const projects = [
       des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
       img: "/p2.svg",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/zoom-clone",
+      link: "https://github.com/SKB336/work-timer",
     },
     {
       id: 3,
@@ -91,7 +91,7 @@ export const projects = [
       des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
       img: "/p3.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
+      link: "",
     },
     {
       id: 4,
@@ -99,7 +99,7 @@ export const projects = [
       des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
       img: "/p4.svg",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
+      link: "",
     },
 ];
   
@@ -112,27 +112,27 @@ export const testimonials = [
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Mr. Shakeeb Ahmed demonstrated excellent problem-solving abilities and a strong understanding of ERP systems. He was proactive in identifying potential issues and addressing them before they escalated. His strong communication skills and teamwork were instrumental in delivering projects on time and exceeding expectations.",
+      name: "Abdulmohsen Alsalman",
+      title: "CEO of Rwasem",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Mr. Shakeeb Ahmed demonstrated excellent problem-solving abilities and a strong understanding of ERP systems. He was proactive in identifying potential issues and addressing them before they escalated. His strong communication skills and teamwork were instrumental in delivering projects on time and exceeding expectations.",
+      name: "Abdulmohsen Alsalman",
+      title: "CEO of Rwasem",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Mr. Shakeeb Ahmed demonstrated excellent problem-solving abilities and a strong understanding of ERP systems. He was proactive in identifying potential issues and addressing them before they escalated. His strong communication skills and teamwork were instrumental in delivering projects on time and exceeding expectations.",
+      name: "Abdulmohsen Alsalman",
+      title: "CEO of Rwasem",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Mr. Shakeeb Ahmed demonstrated excellent problem-solving abilities and a strong understanding of ERP systems. He was proactive in identifying potential issues and addressing them before they escalated. His strong communication skills and teamwork were instrumental in delivering projects on time and exceeding expectations.",
+      name: "Abdulmohsen Alsalman",
+      title: "CEO of Rwasem",
     },
 ];
   
